fb979cffa1310c70c8e4972f2cf914c007e35eb1
RavshanbekBF <ravshanbekbegmurodov@gmail.com>
2025-08-29 14:16:07 +0500

    html

---
+++ first.js
@@ -0,0 +1,11 @@
+//alert('Assalomu alaykum Xo`jayin! ');
+let ism = 'Ravshanbek'
+let yosh = 25
+console.log(ism, yosh)
+ism = 'Bunyodbek'
+yosh = 16
+console.log(ism, yosh)
+const surname = 'Begmurodov'
+console.log(surname)
+var place = 'Namangan'
+console.log(place)
